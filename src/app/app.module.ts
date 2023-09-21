534818015acd7e00262ce2d15afc4fcfe8243146
Carolina4-lang <carolinacastillo1d@gmail.com>
2023-09-21 12:06:46 -0500

    aprendiendo eventos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component'; //importo el typescript
+import { ContadorComponent } from './contador/contador.component';
+import { BotonesComponent } from './botones/botones.component'; //importo el typescript
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    BotonesComponent
   ],
   imports: [
     BrowserModule
