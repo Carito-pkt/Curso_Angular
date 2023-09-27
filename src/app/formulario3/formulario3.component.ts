c184ad66b1039229d602f0698ef69cd96287f8e6
Carolina4-lang <carolinacastillo1d@gmail.com>
2023-09-27 12:05:46 -0500

    validaciones

--- src/app/formulario3/formulario3.component.ts
+++ src/app/formulario3/formulario3.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl} from '@angular/forms';
+import { Form, FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -8,6 +8,22 @@ import { FormControl} from '@angular/forms';
   styleUrls: ['./formulario3.component.css']
 })
 export class Formulario3Component {
-  name = new FormControl('');
-  email = new FormControl('');
+
+  get getName(){
+    return this.formUser.get('name') as FormControl;
+  }
+
+  get getEmail(){
+    return this.formUser.get('email') as FormControl;
+  }
+
+  formUser = new FormGroup({
+    'name': new FormControl('',Validators.required),
+    'email': new FormControl('', [Validators.email, Validators.required])
+  });
+
+  procesar(){
+    console.log(this.formUser.value)
+  }
+
 }
